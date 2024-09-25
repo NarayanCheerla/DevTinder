92b8eb80e305c5c1a2a2553a49dbb2ba3d706d91
CNR <narayana.cheerla@gmail.com>
2024-09-25 18:13:11 +0530

    crud apis

--- app.js
+++ app.js
@@ -1,24 +1,80 @@
 const express = require("express");
-const connectDB = require("./config/database");
+
 const User = require("./models/user");
+const connectDB = require("./config/database");
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+app.get("/user", async (req, res) => {
+    const emailId = req.body.emailId;
+    try {
+        const users = await User.findOne({ emailId });
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Error while finding user .." + err.message);
+    }
+});
+
+app.patch("/user", async (req, res) => {
+    const user = req.body;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(user._id, user, {
+            returnDocument: "after"
+        });
+        if (!updatedUser) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(updatedUser);
+        }
+    } catch (err) {
+        res.status(400).send("Error while updating user .." + err.message);
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send("User delted successfully");
+        }
+
+    } catch (err) {
+        res.status(400).send("Error while deleting user .." + err.message);
+    }
+})
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        if (users.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Error while fetching all users .." + err.message);
+    }
+});
+
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Yuvraj",
-        lastName: "Singh",
-        emailId: "uv@abc.com",
-        password: "test@1234"
-    });
-    try{
+    const user = new User(req.body);
+    try {
         await user.save();
         res.send("User added successfull..");
-    }catch(err){
-        res.status(400).send("Error while saving user.."+err.message);
+    } catch (err) {
+        res.status(400).send("Error while saving user.." + err.message);
     }
-    
+
 });
 
 connectDB().then(() => {
