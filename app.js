a2d5464246499340fe6fef154858caefcb8b6b08
CNR <narayana.cheerla@gmail.com>
2024-09-25 16:28:14 +0530

    first api creation

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
 const PORT = 3000;
 
-app.use("/", (req, res) => {
-    res.send("Server listening");
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Yuvraj",
+        lastName: "Singh",
+        emailId: "uv@abc.com",
+        password: "test@1234"
+    });
+    try{
+        await user.save();
+        res.send("User added successfull..");
+    }catch(err){
+        res.status(400).send("Error while saving user.."+err.message);
+    }
+    
 });
 
 connectDB().then(() => {
--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb://localhost:27017/");
+    await mongoose.connect("mongodb://localhost:27017/devTinder");
 }
 
 module.exports = connectDB;
